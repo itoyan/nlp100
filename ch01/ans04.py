07bf652b15dba1f84075cbe34bd8fcda5d32b494
itoyan <1mail2itoh3@gmail.com>
2015-10-14 01:02:23 +0900

    第1章の解答 20151014
    問題4の文字列処理

---
+++ ch01/ans04.py
@@ -0,0 +1,20 @@
+﻿#-*- coding:utf-8 -*-
+
+'''
+04. 元素記号
+'''
+
+original = "Hi He Lied Because Boron Could Not Oxidize Fluorine. " \
+    "New Nations Might Also Sign Peace Security Clause. Arthur King Can."
+wordList = original.replace(".", "").split(" ")
+
+# wordListから1文字目だけ取り出す
+single = [1, 5, 6, 7, 8, 9, 15, 16, 19]
+
+chemicalSymbol = {}
+
+for word, idx in zip(wordList, range(1, len(wordList))):
+    length = 1 if idx in single else 2 # 3項演算子
+    chemicalSymbol.update({word[0:length]:idx})
+
+print chemicalSymbol
