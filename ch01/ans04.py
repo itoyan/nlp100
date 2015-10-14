c548eeba86026df58a058100f5629de0f0af71b0
itoyan <1mail2itoh3@gmail.com>
2015-10-14 17:47:36 +0900

    問題4のコメントを修正

--- ch01/ans04.py
+++ ch01/ans04.py
@@ -8,13 +8,14 @@ original = "Hi He Lied Because Boron Could Not Oxidize Fluorine. " \
     "New Nations Might Also Sign Peace Security Clause. Arthur King Can."
 wordList = original.replace(".", "").split(" ")
 
-# wordListから何番目の配列の要素だけを1文字目だけ取り出すか指定
+# 配列の中から1文字目だけ取り出す添字(1から始まる)を指定
 single = [1, 5, 6, 7, 8, 9, 15, 16, 19]
 
 chemicalSymbol = {}
 
-for word, idx in zip(wordList, range(1, len(wordList))):
-    length = 1 if idx in single else 2 # 3項演算子
-    chemicalSymbol.update({word[0:length]:idx})
+# for word, idx in zip(wordList, range(1, len(wordList))):
+for idx, word in enumerate(wordList):
+    length = 1 if idx+1 in single else 2 # 3項演算子
+    chemicalSymbol.update({word[0:length]:idx+1})
 
 print chemicalSymbol
