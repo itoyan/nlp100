fa2e35704eb6f504b6e9ab31f02e63d35982c75c
itoyan <1mail2itoh3@gmail.com>
2015-10-22 00:28:27 +0900

    10. 行数のカウントの解答 20151022

---
+++ ch02/ans10.py
@@ -0,0 +1,17 @@
+﻿#-*- coding:utf-8 -*-
+
+'''
+10. 行数のカウント
+    コマンドで確認するときは次のようにする
+    $ wc -l hightemp.txt
+'''
+
+import io
+
+f = io.open('hightemp.txt', 'r',  encoding='utf_8_sig')
+nLine = 0
+for line in f:
+    nLine += 1
+
+f.close()
+print nLine
